9545b7232215201a0b20fa7beff971848fa54ac4
h-ooura <cm6@outlook.jp>
2022-07-23 03:33:06 +0000

    07課題

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,11 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>はじめまして</title>
-    </head>
-    <body>
-        <h1>wellcome</h1>
-    </body>
-</html>
+@extends('layouts.profile')
+@section('title', 'はじめまして')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <center><h2>well come</h2>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create'); # 追記
 });
 Route::group(['prefix' => 'xxx'], function() {
     Route::get( 'Admin\AAAController@bbb');
 });
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
      //Route::get('profile/create', 'Admin\ProfileController@add');
-     Route::get('profile/edit', 'Admin\ProfileController@edit');
+     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
     
 });
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes();
 
- 
+Route::get('/home', 'HomeController@index')->name('home');
+
